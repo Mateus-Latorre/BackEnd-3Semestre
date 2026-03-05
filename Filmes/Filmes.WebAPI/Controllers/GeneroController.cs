14a8c6450add8f578906763b9c44c8eb9c30ac37
MATEUS LATORRE <50055487858@info.local>
2026-03-05 08:42:19 -0300

    Finalizada a API de Filmes

--- Filmes/Filmes.WebAPI/Controllers/GeneroController.cs
+++ Filmes/Filmes.WebAPI/Controllers/GeneroController.cs
@@ -16,7 +16,28 @@ public class GeneroController : ControllerBase
         _generoRepository = generoRepository;
     }
 
+    [HttpGet]
+    public IActionResult Get()
+    {
+        try
+        {
+            return Ok(_generoRepository.Listar());
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+    [HttpGet("{id}")]
 
+    public IActionResult GetById(Guid id)
+    {
+        try { return Ok(_generoRepository.BuscarPorId(id)); }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
     [HttpPost]
 
     public IActionResult Post(Genero novoGenero)
@@ -29,6 +50,46 @@ public class GeneroController : ControllerBase
         catch (Exception ex)
         {
             return BadRequest(ex.Message);
+        }
+    }
+    [HttpPut("{id}")]
+    public IActionResult Put(Guid id, Genero generoAtualizado)
+    {
+        try
+        {
+            _generoRepository.AtualizarIdUrl(id, generoAtualizado);
+            return NoContent();
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpPut]
+    public IActionResult PutBody(Genero generoAtualizado)
+    {
+        try
+        {
+            _generoRepository.AtualizarIdCorpo(generoAtualizado);
+            return NoContent();
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+    [HttpDelete("{id}")]
+    public IActionResult Delete(Guid id)
+    {
+        try
+        {
+            _generoRepository.Deletar(id);
+            return NoContent();
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
         }
     }
 }
--- Filmes/Filmes.WebAPI/obj/Debug/net10.0/Filmes.WebAPI.AssemblyInfo.cs
+++ Filmes/Filmes.WebAPI/obj/Debug/net10.0/Filmes.WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Filmes.WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78fc8e32c8abeb4b4d39d5f6e6d11be5f5e007e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Filmes.WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Filmes.WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
